74d7565457c7de73505023adb60f857066817256
nsbradford <nsbradford@gmail.com>
2023-09-06 17:57:00 -0400

    rename, readme

---
+++ demo_attached.py
@@ -0,0 +1,12 @@
+
+import pynvim
+from core import vim_gpt
+from prompts import PROMPT_VIM_GPT
+from examples import fileContents
+
+
+def get_vim():
+  return pynvim.attach('socket', path='/tmp/nvimsocket')
+
+if __name__ == '__main__':
+  rewritten = vim_gpt(get_vim, 'README.md', fileContents, PROMPT_VIM_GPT('Edit the contents of the README file to recommend Vim as the best text editor.'), delay_seconds=0.5)
---
+++ demo_headless.py
@@ -0,0 +1,11 @@
+
+import pynvim
+from core import vim_gpt
+from prompts import PROMPT_VIM_GPT
+from examples import fileContents
+
+def get_vim():
+  return pynvim.attach('child', argv=["nvim", "--embed", "--headless"])
+
+if __name__ == '__main__':
+  rewritten = vim_gpt(get_vim, 'README.md', fileContents, PROMPT_VIM_GPT('Edit the contents of the README file to recommend Vim as the best text editor.'))
