04d315268c4315fb2c76650a9f9490a16dcc6de1
nsbradford <nsbradford@gmail.com>
2023-09-13 13:29:22 -0400

    try

--- vimgpt/llm.py
+++ vimgpt/llm.py
@@ -8,26 +8,30 @@ import litellm
 
 #=========================================================================
 # TODO promptlayer is not working, have tried a few variations
-import promptlayer
-promptlayer.api_key = os.environ.get("PROMPTLAYER_API_KEY")
-openai = promptlayer.openai  # type: ignore
+
+# this was the old code before litellm:
+# import promptlayer
+# promptlayer.api_key = os.environ.get("PROMPTLAYER_API_KEY")
+# openai = promptlayer.openai  # type: ignore
 
 litellm.success_callback = ["promptlayer"]
 
 
 #=========================================================================
 # TODO, helicone cache is NOT working.
-# litellm.api_url = "https://oai.hconeai.com/v1"
+litellm.api_url = "https://oai.hconeai.com/v1"
 
-# litellm.headers = {
-#     "Helicone-Auth": f"Bearer {os.getenv('HELICONE_API_KEY')}",
-#     "Helicone-Cache-Enabled": "true",
-# }
+litellm.headers = {
+    "Helicone-Auth": f"Bearer {os.getenv('HELICONE_API_KEY')}",
+    "Helicone-Cache-Enabled": "true",
+}
 
 logger = logging.getLogger(__name__)
 
 def llm_get_keystrokes(model: str, messages: List[Dict[str, str]]) -> str:
-    logger.warning(f"Calling LLM {model}") # with: {messages}
+    logger.warning(f"Calling LLM {model}")
+
+    # this was the old code before litellm:
     # chat_completion = openai.ChatCompletion.create(
     #     model="gpt-4",
     #     temperature=0,
@@ -41,6 +45,7 @@ def llm_get_keystrokes(model: str, messages: List[Dict[str, str]]) -> str:
     # )
     # text = chat_completion.choices[0].message.content
     # logger.warning(f"LLM response: {text}")
+    
     litellm_completion = litellm.completion(model=model, messages=messages, temperature=0)
     text = litellm_completion.choices[0].message.content
     logger.warning(f"LiteLLM response: {litellm_completion.choices[0].message.content}")
