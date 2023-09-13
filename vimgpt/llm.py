780b7e70c961d29a8bac9387c80fc898ff1bbc37
nsbradford <nsbradford@gmail.com>
2023-09-13 17:41:13 -0400

    remove stuff

--- vimgpt/llm.py
+++ vimgpt/llm.py
@@ -5,16 +5,6 @@ from typing import Dict, List
 import litellm
 from litellm import completion
 
-# import openai
-
-# =========================================================================
-# TODO promptlayer is not working, have tried a few variations
-
-# this was the old code before litellm:
-# import promptlayer
-# promptlayer.api_key = os.environ.get("PROMPTLAYER_API_KEY")
-# openai = promptlayer.openai  # type: ignore
-
 # https://docs.litellm.ai/docs/observability/promptlayer_integration
 litellm.success_callback = ["promptlayer"]
 
@@ -34,18 +24,3 @@ def llm_get_keystrokes(model: str, messages: List[Dict[str, str]]) -> str:
     text = litellm_completion.choices[0].message.content
     logger.warning(f"LiteLLM response: {litellm_completion.choices[0].message.content}")
     return text
-
-    # this was the old code before litellm:
-    # chat_completion = openai.ChatCompletion.create(
-    #     model="gpt-4",
-    #     temperature=0,
-    #     messages=messages,
-    #     api_key=os.environ["OPENAI_API_KEY"],
-    #     api_base="https://oai.hconeai.com/v1",
-    #     headers={
-    #         "Helicone-Auth": f"Bearer {os.environ['HELICONE_API_KEY']}",
-    #         "Helicone-Cache-Enabled": "true",
-    #     },
-    # )
-    # text = chat_completion.choices[0].message.content
-    # logger.warning(f"LLM response: {text}")
